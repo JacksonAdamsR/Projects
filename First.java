89e1b2bd3dc325d5a67bbf60bf80bc716c44a813
JacksonAdamsR <adamsjackson1220@gmail.com>
2025-01-20 21:22:05 -0500

    Add files via upload

---
+++ First.java
@@ -0,0 +1,11 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Job Title: Information Security Analysts");
+        System.out.println("Why: High pay, only needs bachelors, aligns with major and interests");
+        System.out.println("Median salary: 120,000");
+
+
+
+    }
+}
