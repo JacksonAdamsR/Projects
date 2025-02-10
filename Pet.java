5864a62b000f8f846cc25b7454b52bdb6c21224b
JacksonAdamsR <adamsjackson1220@gmail.com>
2025-02-09 23:30:35 -0500

    Add files via upload

--- Pet.java
+++ Pet.java
@@ -1,38 +1,61 @@
 public class Pet {
-    
     private String name;
+    private String type;
+    private int age;
 
-    
     public Pet() {
-        this.name = "Pet Name"; 
+        this.name = "";
+        this.type = "";
+        this.age = 0;
     }
 
-    
-    public void setName(String name) {
-        this.name = name;
+    public Pet(String name, String type, int age) {
+        setName(name);
+        setType(type);
+        setAge(age);
     }
 
-    
     public String getName() {
         return name;
     }
 
-    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String speak() {
+        if (type.equalsIgnoreCase("dog")) {
+            return "Woof";
+        } else if (type.equalsIgnoreCase("cat")) {
+            return "Meow";
+        } else {
+            return "Noise";
+        }
+    }
+
     @Override
     public String toString() {
-        return "Pet information:\nName: " + name;
-    }
-
-    
-    public static void main(String[] args) {
-        
-        Pet pet1 = new Pet();
-        System.out.println(pet1);
-
-        
-        Pet pet2 = new Pet();
-        pet2.setName("Buster");
-        System.out.println(pet2);
+        String result = "Pet information:\n";
+        result += "Type: " + getType() + "\n";
+        result += "Name: " + getName() + "\n";
+        result += "Sound: " + speak() + "\n";
+        result += "Age: " + getAge() + "\n";
+        return result;
     }
 }
-
